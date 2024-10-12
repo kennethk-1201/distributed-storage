fa872ad613470139023bfab773efe40ceb30c9a3
Kenneth Kiang <kiangjiaming@gmail.com>
2024-10-12 16:04:00 +0800

    first commit

---
+++ p2p/tcp_transport.go
@@ -0,0 +1,96 @@
+package p2p
+
+import (
+	"fmt"
+	"net"
+	"sync"
+)
+
+/*
+
+Notes:
+- Organise your code such that public functions are at the top, private functions are
+at the bottom.
+- More important functions should be at the top.
+
+*/
+
+type TCPPeer struct {
+	conn net.Conn
+
+	// if we dial and retrieve a conn => outbound == true
+	// if we accept and  retrieve a conn => outbound == false
+	outbound bool
+}
+
+func NewTCPPeer(conn net.Conn, outbound bool) *TCPPeer {
+	return &TCPPeer{
+		conn:     conn,
+		outbound: outbound,
+	}
+}
+
+type TCPTransport struct {
+	listenAddress string
+	listener      net.Listener
+	shakeHands    HandshakeFunc
+	decoder       Decoder
+
+	mu    sync.RWMutex // common practice to put mutex field above what it is protecting
+	peers map[net.Addr]Peer
+}
+
+func NewTCPTransport(listenAddr string) *TCPTransport {
+	return &TCPTransport{
+		shakeHands:    NOPHandshakeFunc,
+		listenAddress: listenAddr,
+	}
+}
+
+func (t *TCPTransport) ListenAndAccept() error {
+
+	var err error
+
+	t.listener, err = net.Listen("tcp", t.listenAddress)
+	if err != nil {
+		return err
+	}
+
+	go t.startAcceptLoop()
+
+	return nil
+}
+
+func (t *TCPTransport) startAcceptLoop() {
+	for {
+		conn, err := t.listener.Accept()
+		if err != nil {
+			fmt.Printf("TCP accept error: %s\n", err)
+		}
+
+		fmt.Printf("new incoming connection %+v\n", conn)
+
+		go t.handleConn(conn)
+	}
+}
+
+type Temp struct{}
+
+func (t *TCPTransport) handleConn(conn net.Conn) {
+	peer := NewTCPPeer(conn, true)
+
+	if err := t.shakeHands(peer); err != nil {
+
+	}
+
+	// Read Loop
+	msg := &Temp{}
+	for {
+		if err := t.decoder.Decode(conn, msg); err != nil {
+			fmt.Printf("TCP error: %s\n", err)
+			continue
+		}
+	}
+
+	fmt.Printf("new incoming connection %+v\n", conn)
+}
